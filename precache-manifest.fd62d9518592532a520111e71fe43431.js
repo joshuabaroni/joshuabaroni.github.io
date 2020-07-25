e44e4162bf3afcfd040c5c1d335417f6d2a4ded1
Joshua Baroni <joshua.baroni.16@cnu.edu>
2020-07-25 13:30:42 -0400

    Updates

---
+++ precache-manifest.fd62d9518592532a520111e71fe43431.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5151c5f4114dfa79e680b57738bad585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2159820996ff1099a47e",
+    "url": "/static/css/main.9a028e43.chunk.css"
+  },
+  {
+    "revision": "098d2de3d6eea89bd3c9",
+    "url": "/static/js/2.ebbd51ed.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ebbd51ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2159820996ff1099a47e",
+    "url": "/static/js/main.e0a40d22.chunk.js"
+  },
+  {
+    "revision": "e7b2d03c696f961683de",
+    "url": "/static/js/runtime-main.cf3e5fee.js"
+  },
+  {
+    "revision": "5bae192acfc1d8cf9f87e790c22ca5f9",
+    "url": "/static/media/ProfilePicBaroni2016.5bae192a.jpg"
+  }
+]);
